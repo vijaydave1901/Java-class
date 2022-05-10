b057b86115cc2823b74cf1b0809d495ad2a221c6
vijay Dave <vijaydave1901@gmail.com>
2022-05-10 13:18:21 -0400

    Hello World in Netbeans

--- Hello.java
+++ Hello.java
@@ -1,10 +1,8 @@
 
-public class Hello
-	{
+public class Hello	{
 		public static void main(String args [] )
 		{
 			System.out.println("Hello World");
-			System.out.println("Hello Developer now I'm Using Git to save my code ..")
 		}
 
 }
---
+++ HelloWorld/src/main/java/com/mthree/helloworld/HelloNetBeans.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+* author Vijay Prakash Dave
+* Date 10th May 2022
+* version 1.0
+ */
+package com.mthree.helloworld;
+
+/**
+ *
+ * @author abc
+ */
+public class HelloNetBeans {
+    
+    public static void main(String args [])
+    {
+        System.out.println("Hello World ..");
+    }
+    
+}
